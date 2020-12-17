74bd5e7739e914701e4d3df9cd4fb9563b99dadc
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2020-12-17 16:16:37 +0100

    Update Test

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,7 @@
 <template>  
-<<<<<<< HEAD
+
     <div class="absolute top-0 bg-blue w-1/3 h-screen">
-=======
-    <div class="test absolute top-0 bg-blue w-1/3 h-screen animate-openModal">
->>>>>>> parent of 03b5d71... Update Style
+
 
         <div class="mt-4 pr-7 w-full flex items-center justify-end cursor-pointer" @click="close()">
             <span class="material-icons text-4xl text-white">close</span>
@@ -37,7 +35,3 @@ export default {
     }
 }
 </script>
-
-<style>
-
-</style>
