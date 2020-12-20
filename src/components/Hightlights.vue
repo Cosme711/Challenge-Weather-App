1d79058238aa6da5e04648376e9d9a8fd59a4117
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2020-12-20 16:53:05 +0100

    Update Hightlights grid

--- src/components/Hightlights.vue
+++ src/components/Hightlights.vue
@@ -2,34 +2,32 @@
   <div class="text-white">
       <p class="mt-14 text-3xl font-bold">Today’s Hightlights </p>
 
-      <div class="mt-8 flex text-center">
-          <div class="w-full mr-6 bg-blue">
-              <p class="mt-6 mb-2 text-lg">Wind status</p>
-              <p class="text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.wind_speed) }}</span>mph</p>
-              <div class="m-8 flex justify-center items-center">
+    <div class="mt-8 grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 gap-5 lg:gap-10 text-center">
+          <div class=" bg-blue">
+              <p class="mt-4 text-lg">Wind status</p>
+              <p class="mt-3 text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.wind_speed) }}</span>mph</p>
+              <div class="mt-6 mb-4 mx-8 flex justify-center items-center">
                   <div class="bg-grey rounded-full w-8 h-8 flex items-center justify-center">
                         <span class="material-icons text-lg">navigation</span>
                   </div>
                   <p class="ml-4">WSW</p>
               </div>
           </div>
-          <div class="w-full ml-6 bg-blue">
-              <p class="mt-6 mb-4 text-lg">Humidity</p>
-              <p class="text-3xl "><span class="font-bold text-7xl">{{ Math.round(current.weather.humidity) }}</span>%</p>
-          </div>
-      </div>
-
-      <div class="mt-16 flex text-center">
-          <div class="w-full mr-6 bg-blue">
-                <p class="mt-6 mb-2 text-lg">Visibility</p>
-                <p class="mb-10 text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.visibility) }}</span>miles</p>
+          <div class="bg-blue">
+              <p class="mt-4 text-lg">Humidity</p>
+              <p class="mt-3 text-3xl "><span class="font-bold text-7xl">{{ Math.round(current.weather.humidity) }}</span>%</p>
           </div>
 
-          <div class="w-full ml-6 bg-blue">
-                <p class="mt-6 mb-2 text-lg">Air Pressure</p>
-                <p class=" mb-10 text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.air_pressure) }}</span>mb</p>
+          <div class="bg-blue">
+                <p class="mt-4 text-lg">Visibility</p>
+                <p class="mt-3 text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.visibility) }}</span>miles</p>
           </div>
-      </div>
+
+          <div class="bg-blue">
+                <p class="mt-4 text-lg">Air Pressure</p>
+                <p class="mt-3 text-3xl"><span class="font-bold text-7xl">{{ Math.round(current.weather.air_pressure) }}</span>mb</p>
+          </div>
+    </div>
 
       <footer class="mt-5 text-grey text-center">
           <p>Cosme Gressier | <a href="">DevChallenges</a></p>
