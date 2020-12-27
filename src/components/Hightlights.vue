ce77fdf3e330af6efb389aa34cccd6f1c6174efa
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2020-12-27 18:38:25 +0100

    Update Footer

--- src/components/Hightlights.vue
+++ src/components/Hightlights.vue
@@ -40,8 +40,10 @@
           </div>
     </div>
 
-      <footer class="mt-16 text-center text-footer">
-          <p>Cosme Gressier | <a href="">DevChallenges</a> | @ DevChallenges.io</p>
+      <footer class="mt-16 flex flex-col sm:flex-row justify-center text-center text-footer">
+          <div><a href="https://github.com/Cosme711">Cosme Gressier</a></div>
+          <div class="mx-2"><p>| <a href="https://devchallenges.io/">DevChallenges</a> |</p></div>
+          <div><a href="https://twitter.com/GressierCosme1">@GressierCosme1</a></div>
       </footer>
   </div>
 </template>
