eccd24c0b58426603e3a11f4fbdacc311bbd5275
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2021-01-23 18:15:06 +0100

    Revert "Update Footer"
    This reverts commit 270961de73e71fa45911f1c0f68e3d4476d97eb3.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="flex flex-col sm:flex-row justify-center text-center text-footer">
-        <div><a href="https://www.metaweather.com">MetaWeather API</a></div>
+        <div><a href="https://github.com/Cosme711">Cosme Gressier</a></div>
         <div class="mx-2"><p>| <a href="https://devchallenges.io/">DevChallenges</a> |</p></div>
         <div><a href="https://twitter.com/GressierCosme1">@GressierCosme1</a></div>
     </footer>
